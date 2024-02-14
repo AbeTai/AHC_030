5ad00fdc28440f4d7af5758d1bd2819ad9884351
abetaichi <taichi.sept.28@gmail.com>
2024-02-14 18:31:41 +0900

    期待値，クロス占い

--- test/20240214_155707_log.py
+++ test/20240214_155707_log.py
@@ -10,6 +10,7 @@
 """
 from collections import Counter
 import sys
+import copy
 ### ベース部分
 # read prior information
 line = input().split()
@@ -50,7 +51,7 @@ for pol in fields:
     max_polyomino_y = max([x[1] for x in pol])
     low_move = N - max_polyomino_x
     light_move = N - max_polyomino_y
-    print(low_move,light_move, file=sys.stderr)
+    #print(low_move,light_move, file=sys.stderr)
 
     for low in range(low_move):
         for light in range(light_move):
@@ -68,13 +69,74 @@ for pol in fields:
         coordinate_exp[key] += count
 
 #print(coordinate_exp, file=sys.stderr)
-        
+coordinate_exp_idx = copy.deepcopy(coordinate_exp)
 ### 変更部分
+# ある行について占う→その行の期待値を変更，を繰り返す
+for idx in range(N):
+    # 占い
+    fortune_input = [(idx, x) for x in range(N)]
+    print("q {} {}".format(len(fortune_input), ' '.join(map(lambda x: "{} {}".format(x[0], x[1]), fortune_input))))
+    resp = float(input()) # V(s)の近似値を受け取る
+    """
+    if idx == 14:
+        print(resp, file = sys.stderr)
+    """
+
+    # その行の期待値を取り出す
+    ## その行のマスを生成
+    coordinate_idx_tmp = [(idx, col) for col in range(N)]
+    ## その行の期待値の和を計算
+    exp_sum_idx = 0
+    for col in range(N):
+        exp_sum_idx += coordinate_exp[coordinate_idx_tmp[col]]
+    ## 倍率計算
+    num_times = resp / exp_sum_idx
+    ## 期待値にかけ直す
+    for col in range(N):
+        val_raw = coordinate_exp[coordinate_idx_tmp[col]]
+        coordinate_exp_idx[coordinate_idx_tmp[col]] = val_raw * num_times
+
+coordinate_exp_col = copy.deepcopy(coordinate_exp)
+# ある列について占う→その列の期待値を変更，を繰り返す
+for col in range(N):
+    # 占い
+    fortune_input = [(x, col) for x in range(N)]
+    print("q {} {}".format(len(fortune_input), ' '.join(map(lambda x: "{} {}".format(x[0], x[1]), fortune_input))))
+    resp = float(input()) # V(s)の近似値を受け取る
+    """
+    if col == 13:
+        print(resp, file = sys.stderr)
+    """
+
+    # その行の期待値を取り出す
+    ## その行のマスを生成
+    coordinate_col_tmp = [(x, col) for x in range(N)]
+    ## その行の期待値の和を計算
+    exp_sum_col = 0
+    for idx in range(N):
+        exp_sum_col += coordinate_exp[coordinate_col_tmp[idx]]
+    ## 倍率計算
+    num_times = resp / exp_sum_col
+    ## 期待値にかけ直す
+    for idx in range(N):
+        val_raw = coordinate_exp[coordinate_col_tmp[idx]]
+        coordinate_exp_col[coordinate_col_tmp[idx]] = val_raw * num_times
+
+coordinate_exp_idx_col = copy.deepcopy(coordinate_exp)
+
+# 行列占い後の結果をかける
+for ic in coordinate_exp.keys():
+    idx_val = coordinate_exp_idx[ic]
+    col_val = coordinate_exp_col[ic]
+    coordinate_exp_idx_col[ic] = idx_val * col_val
+
+#print(coordinate_exp, file=sys.stderr)
+#print(coordinate_exp_idx_col, file=sys.stderr)
 
 
-# 一旦シンプルに，期待値順に占って，掘り終わるまでやる
+# 一旦シンプルに，行占いで補正した期待値順に占って，掘り終わるまでやる
 # 掘る順番
-dig_order= dict(sorted(coordinate_exp.items(), key=lambda item: item[1], reverse=True)).keys()
+dig_order = dict(sorted(coordinate_exp_idx_col.items(), key=lambda item: item[1], reverse=True)).keys()
 
 has_oil = []
 oil_reserves = 0
